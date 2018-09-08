6b6acf6bf9ee0d1e0837d727958a797069d976e2
William Ku <kucheech@gmail.com>
2018-09-08 22:59:53 +0800

    Add project files.

---
+++ Scenario 2-2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Scenario_2_2
+{
+    class Program
+    {
+        public struct Location
+        {
+            public int x, y, z;
+        }
+
+        static void Main(string[] args)
+        {
+            Location[] data = new Location[5000];
+            Random r = new Random();
+            for(int i = 0; i < data.Length; i++)
+            {
+                data[i].x = r.Next();
+                data[i].y = r.Next();
+                data[i].z = r.Next();
+                Console.WriteLine(data[i].x + ", " + data[i].y + ", " + data[i].z);
+            }
+        }
+    }
+}
